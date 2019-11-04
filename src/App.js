8c0c2d48782149e3ee24104d6af2f079f97a693d
Sam <Jeffcoat>
2019-11-04 15:01:15 -0600

    Imports productContext, wraps app in provider, passes props to provider and refactors route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from './contexts/ProductContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -19,24 +20,21 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value = {{products, addItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route
 				exact
 				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
+				component = {Products}
 			/>
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
